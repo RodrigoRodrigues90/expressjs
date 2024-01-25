bcb0eee52d3f505246691368d46bae04a54ffab3
rodrigo.rodrigues <rodrigorodrigues_0990@hotmail.com>
2024-01-25 13:38:23 -0300

    correção

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,4 +4,8 @@ const router = require('./router');
 app.use(express.json());
 app.use(router);
 app.listen(3333, ()=> console.log("Listening on port 3333"))
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
 module.exports = app
